f0eb2fb1ad7f1dfe886625c19b460d3e30d2c149
Geek-Tekina <tpbc1717@gmail.com>
2024-08-13 13:55:10 +0530

    added auth for info page

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Info = () => {
+  const token = localStorage.getItem('token');
+  console.log(token)
+  if(token)
   return (
     <div>Info</div>
+  )
+  else return (
+    <div>
+      Not Authorized !! 
+      <Link to="/">Go to Login Page </Link>
+    </div>
   )
 }
 
